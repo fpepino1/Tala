a6fc44b105bf31856143735803b9958a16d804e9
FIRE <115742428+fpepino1@users.noreply.github.com>
2024-08-16 06:07:10 -0400

    update messagescreen

--- Archives-mobile/screens/Messages/MessageScreen.tsx
+++ Archives-mobile/screens/Messages/MessageScreen.tsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { View, TextInput, TouchableOpacity, FlatList, Text, StyleSheet, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard, Image } from 'react-native';
+import { View, TextInput, TouchableOpacity, FlatList, Text, StyleSheet, KeyboardAvoidingView, Platform, Keyboard, Image } from 'react-native';
 import { sendMessage, useMessages } from './MessageFunctions';
 import { StackParamList } from '../../navigation/types';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { useNavigation } from '@react-navigation/native';
-import { FIREBASE_DB } from '../../FirebaseConfig';
-import { doc, getDoc, onSnapshot, serverTimestamp, setDoc,  where,  getDocs } from 'firebase/firestore';
+
 const MessageScreen = ({ route }) => {
   const navigation = useNavigation<StackNavigationProp<StackParamList, 'MessageScreen'>>();
   const { userId, chatRoomId, photoUrl, name, username, currentUserId } = route.params;
@@ -17,13 +16,25 @@ const MessageScreen = ({ route }) => {
     const date = timestamp.toDate();  
     return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }); 
   };
+
   useEffect(() => {
     console.log('Chat Room ID:', chatRoomId);
     console.log('User ID:', userId);
     console.log('Current User ID:', currentUserId);
     console.log('Messages:', messages);
   }, [messages]);
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitle: () => (
+     
+        <View style={{marginHorizontal:'auto', alignItems: 'center'}}>
+            <Text style={styles.name}>{name}</Text>
+            <Text style={styles.username}>{username}</Text>
+        </View>
 
+      ),
+    });
+  }, [navigation, photoUrl, name, username]);
   const handleSend = () => {
     if (message.trim()) {
       sendMessage(chatRoomId, currentUserId, userId, message, photoUrl, name, username);  
@@ -32,95 +43,67 @@ const MessageScreen = ({ route }) => {
   };
 
   const reversedMessages = Array.isArray(messages) ? [...messages].reverse() : [];
-  // const goToUserProfile = async () => {
-  //   if (user) {
-  //     try {
-  //       // Fetch the user's posts
-  //       const postsRef = collection(FIREBASE_DB, 'users', uid, 'posts');
-  //       const postsSnapshot = await getDocs(postsRef);
-  
-  //       const userPosts: any[] = postsSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-  
-  //       navigation.navigate('UserProfile', {
-  //         username: user.username,
-  //         name: user.name,
-  //         photoUrl: user.photoUrl,
-  //         bio: user.bio,
-  //         userId: uid,
-  //         posts: userPosts, 
-  //       });
-  //     } catch (error) {
-  //       console.error('Error fetching user posts:', error);
-  //     }
-  //   }
-  // };
-  
-
 
   return (
-    <View style={styles.container}>
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.innerContainer}>
-          <View style={styles.header}>
-            <Image source={{ uri: photoUrl }} style={styles.profileImage} />
-            <View style={styles.headerText}>
-              <Text style={styles.name}>{name}</Text>
-              <Text style={styles.username}>{username}</Text>
-            </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 80 : 20}
+    >
+      <View style={styles.container}>
+        {/* <View style={styles.header}>
+          <Image source={{ uri: photoUrl }} style={styles.profileImage} />
+          <View style={styles.headerText}>
+            <Text style={styles.name}>{name}</Text>
+            <Text style={styles.username}>{username}</Text>
           </View>
+        </View> */}
 
-          <FlatList
-            data={reversedMessages}
-            renderItem={({ item }) => (
+        <FlatList
+          data={reversedMessages}
+          renderItem={({ item }) => (
+            <View style={[
+              styles.messageContainer,
+              item.senderId === currentUserId ? styles.messageSent : styles.messageReceived
+            ]}>
               <View style={[
-                styles.messageContainer,
-                item.senderId === currentUserId ? styles.messageSent : styles.messageReceived
+                styles.messageBubble,
+                { backgroundColor: item.senderId === currentUserId ? '#0d0d0d' : '#d9d9d9' },
               ]}>
-                <View style={[
-                  styles.messageBubble,
-                  { backgroundColor: item.senderId === currentUserId ? '#0d0d0d' : '#d9d9d9' },
-                ]}>
-                  <Text style={[styles.messageText, { color: item.senderId === currentUserId ? '#fff' : '#000' }]}>{item.message}</Text>
-                  <Text style=
-                  {
-                    [
-                      styles.messageTimestamp,
-                     { color: item.senderId === currentUserId ? '#fff' : '#000' }]}>
-              {formatTimestamp(item.timestamp)}
-            </Text>
-                </View>
+                <Text style={[styles.messageText, { color: item.senderId === currentUserId ? '#fff' : '#000' }]}>{item.message}</Text>
+                <Text style={[styles.messageTimestamp, { color: item.senderId === currentUserId ? '#fff' : '#000' }]}>
+                  {formatTimestamp(item.timestamp)}
+                </Text>
               </View>
-            )}
-            keyExtractor={(item) => item.id}
-            inverted
-            contentContainerStyle={{ flexGrow: 1 }}
-          />
-
-          <KeyboardAvoidingView
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-            keyboardVerticalOffset={Platform.OS === "ios" ? 80 : 20}
-          >
-            <View style={styles.inputWrapper}>
-              <TextInput
-                value={message}
-                onChangeText={setMessage}
-                placeholder="Type your message"
-                style={styles.input}
-              />
-              {message.trim().length > 0 && (
-                <TouchableOpacity
-                  style={styles.sendButton}
-                  onPress={handleSend}
-                >
-                  <Text style={styles.sendButtonText}>Send</Text>
-                </TouchableOpacity>
-              )}
             </View>
-          </KeyboardAvoidingView>
+          )}
+          keyExtractor={(item) => item.id}
+          ListEmptyComponent={<Text style={styles.emptyMessage}>Start a conversation</Text>}
+          keyboardDismissMode="on-drag"   
+          inverted
+          showsVerticalScrollIndicator={false}  
+          contentInsetAdjustmentBehavior="automatic"  
+          style={{ flex: 1 }}  
+        />
+
+        <View style={styles.inputWrapper}>
+          <TextInput
+            value={message}
+            onChangeText={setMessage}
+            placeholder="Type your message"
+            style={styles.input}
+          />
+          {message.trim().length > 0 && (
+            <TouchableOpacity
+              style={styles.sendButton}
+              onPress={handleSend}
+            >
+              <Text style={styles.sendButtonText}>Send</Text>
+            </TouchableOpacity>
+          )}
         </View>
-      </TouchableWithoutFeedback>
-      
-    </View>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -129,17 +112,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F8F3FA',
   },
-  innerContainer: {
-    flex: 1,
-    flexDirection: 'column',
-  },
   header: {
     alignItems: 'center',
     padding: '1%',
     paddingHorizontal: '3%',
-    borderBottomWidth: 1,
-    borderBottomColor: '#ddd',
-    flexDirection: 'row',
   },
   profileImage: {
     width: 50,
@@ -209,6 +185,11 @@ const styles = StyleSheet.create({
   sendButtonText: {
     color: '#fff',
     fontWeight: 'bold',
+  },
+  emptyMessage: {
+    textAlign: 'center',
+    marginVertical: 20,
+    color: '#888',
   },
 });
 
